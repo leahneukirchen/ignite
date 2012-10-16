09222a8155f596202eaec311e4005484604ee08b
Christian Neukirchen <chneukirchen@gmail.com>
2012-10-16 17:04:25 +0200

    add pause utility

---
+++ ignite/util/pause.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <signal.h>
+
+static void
+nop(int sig)
+{
+}
+
+int
+main()
+{
+  signal(SIGTERM, nop);
+  signal(SIGINT, nop);
+  signal(SIGHUP, SIG_IGN);
+
+  pause();
+
+  return 0;
+}
